d6904b12dffd50bab98f07b91c3b00b76e6bc1be
yalmaz-ali <yalmaz.alizafar@gmail.com>
2024-04-03 15:04:06 +0500

    Update Django secret key in settings.py

--- emailverification/settings.py
+++ emailverification/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
 load_dotenv(env_path)
 
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ.get('DJANGO_SECRET')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
